1ac9cc90cd2c065e2b2a442fc6de6bb68e39b01e
Hayley <37635523+greenough-h@users.noreply.github.com>
2023-12-12 16:52:17 -0500

    JS weather data update

--- src/index.js
+++ src/index.js
@@ -5,17 +5,30 @@ function citySearch(event) {
 
   let apiKey = "t0cee57ed010o387a24333e4fba6d54e";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(tempUpdate);
+  axios.get(apiUrl).then(update);
 }
 
-function tempUpdate(response) {
+function update(response) {
   let temperatureElement = document.querySelector(".temp");
   let temperature = Math.round(response.data.temperature.current);
-  temperatureElement.innerHTML = temperature;
 
   let cityElement = document.querySelector("#city");
   let city = response.data.city;
+
+  let conditionElement = document.querySelector("#cond");
+  let condition = response.data.condition.description;
+
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+
+  temperatureElement.innerHTML = temperature;
   cityElement.innerHTML = city;
+  conditionElement.innerHTML = condition;
+  windElement.innerHTML = `${wind} km/h`;
+  humidityElement.innerHTML = `${humidity}%`;
 }
 
 let searchElement = document.querySelector(".search-form");
