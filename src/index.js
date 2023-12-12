47c7845de587a047263271fff490d6812cdaa7ca
Hayley <37635523+greenough-h@users.noreply.github.com>
2023-12-12 16:41:56 -0500

    Axios API connect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-function cityUpdate(event) {
+function citySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-input");
-  let cityInput = searchInput.value;
+  let city = searchInput.value;
+
+  let apiKey = "t0cee57ed010o387a24333e4fba6d54e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(tempUpdate);
+}
+
+function tempUpdate(response) {
+  let temperatureElement = document.querySelector(".temp");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
 
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = cityInput;
+  let city = response.data.city;
+  cityElement.innerHTML = city;
 }
 
 let searchElement = document.querySelector(".search-form");
-searchElement.addEventListener("submit", cityUpdate);
-
-let apiKey = "t0cee57ed010o387a24333e4fba6d54e";
-console.log(apiKey);
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
+searchElement.addEventListener("submit", citySearch);
